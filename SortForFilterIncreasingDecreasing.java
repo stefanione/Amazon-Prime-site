1fce206cf3684a03dcd7c945b22024aa84016b15
stefanione <94955851+stefanione@users.noreply.github.com>
2023-01-09 20:08:28 +0200

    Clasele de sortare
    Fiecare dintre aceste clase este folosita pentru sortare : IncreasingIncreasing, IncreasingDecreasing, DecreasingIncreasing, DecreasingDecreasing. Toate sunt folosite si apelate in decursul actiunii de "filters". Folosesc Bubble Sort pentru a sorta in functie de Duration si Rating. Dupa cum era mentionat in cerinta, intai dupa duration iar in caz de egalitate, dupa rating, in caz de ambele capuri sunt date de input.

---
+++ SortForFilterIncreasingDecreasing.java
@@ -0,0 +1,39 @@
+package fileio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortForFilterIncreasingDecreasing implements SortingForFilterIncreasingDecreasing,
+        InterfaceForfilterIncreasingDecreasing {
+
+    /** this method sorts the array corrspondly */
+    @Override
+    public void sortFilterIncreasingDecreasing(
+            final ArrayList<InputforMovies> remainingmovies) {
+        int sorted = 0;
+        while (sorted == 0) {
+            sorted = 1;
+            for (int u = 0; u < remainingmovies.size() - 1; u++) {
+                if (remainingmovies.get(u).getDuration()
+                        < remainingmovies.get(u + 1).getDuration()) {
+                    sorted = 0;
+                    Collections.swap(remainingmovies, u, u + 1);
+
+                } else if (remainingmovies.get(u).getDuration()
+                        == remainingmovies.get(u + 1).getDuration()) {
+                    if (remainingmovies.get(u).getRating()
+                            > remainingmovies.get(u + 1).getRating()) {
+                        sorted = 0;
+                        Collections.swap(remainingmovies, u + 1, u);
+                    }
+                }
+            }
+        }
+    }
+
+    /** this method prints that the arraylist has been sorted the correspondent way */
+    @Override
+    public void sortFilterIncreasingDecreasing() {
+        System.out.println("sortedIncreasingDecreasing");
+    }
+}
